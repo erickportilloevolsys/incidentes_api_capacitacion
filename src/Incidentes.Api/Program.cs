36597933397d69765c286ca73860c3310d6981e9
Erick Portillo <eriporti22.5@gmail.com>
2025-08-26 19:31:01 -0500

    Añadir soporte para Swagger y archivos estáticos

--- src/Incidentes.Api/Program.cs
+++ src/Incidentes.Api/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<ITipoIncidenteService, TipoIncidenteService>();
 builder.Services.AddScoped<IPrioridadService, PrioridadService>();
 builder.Services.AddScoped<IImpactoService, ImpactoService>();
@@ -30,9 +32,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    app.UseHttpsRedirection();
 
 app.UseRouting();
 
