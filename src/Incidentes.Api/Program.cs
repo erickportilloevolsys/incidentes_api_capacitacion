1ac5a7431da3064499207a3a4da4acff71e6c96d
Erick Portillo <eriporti22.5@gmail.com>
2025-08-28 10:04:38 -0500

    Añadir política de CORS en Program.cs
    Se ha implementado una política de CORS que permite cualquier encabezado, método y origen. Esta configuración se aplica en la tubería de la aplicación después de la redirección HTTPS y antes de la autorización.

--- src/Incidentes.Api/Program.cs
+++ src/Incidentes.Api/Program.cs
@@ -23,6 +23,16 @@ builder.Services.AddScoped<IIncidenteRepository, IncidenteRepository>();
 
 builder.Services.AddSqlServer<AppDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,6 +51,7 @@ else
     app.UseHttpsRedirection();
 
 app.UseRouting();
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
